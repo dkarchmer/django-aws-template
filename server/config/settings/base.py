28de14ba7a2f0c1e0fcfa2b9e0097e6920195b9c
Justin Headley <jheadley@archsys.io>
2019-01-17 08:31:11 -0800

    Fixes bug caused by newer django versions. See:
    https://stackoverflow.com/questions/25879725/django-improperlyconfigured-module-django-contrib-auth-middleware

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -127,7 +127,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'apps.utils.timezoneMiddleware.TimezoneMiddleware',
