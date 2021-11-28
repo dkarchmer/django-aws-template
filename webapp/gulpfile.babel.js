f037447feef524243f4791fa7a7028e0a56dc831
David Karchmer <dkarchmer@gmail.com>
2021-11-27 23:43:00 -0800

    Cleanup gulp flow

--- webapp/gulpfile.babel.js
+++ webapp/gulpfile.babel.js
@@ -1,17 +1,14 @@
 // generated on 2016-03-23 using generator-webapp 2.0.0
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
 import del from 'del';
 import {stream as wiredep} from 'wiredep';
 import replace from 'gulp-replace';
 var gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
 
 const config = {
   htmlFiles: 'src/*.html',
-  imageFiles: 'src/app/images/**/*',
   fontFiles: 'src/app/fonts/**/*',
   scssFiles: 'src/app/styles/*.scss',
   jsFiles: 'src/app/scripts/**/*.js',
@@ -33,8 +30,7 @@ gulp.task('styles', () => {
     }).on('error', $.sass.logError))
     .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/app/styles'))
-    .pipe(reload({stream: true}));
+    .pipe(gulp.dest('.tmp/app/styles'));
 });
 
 gulp.task('scripts', () => {
@@ -43,39 +39,8 @@ gulp.task('scripts', () => {
     .pipe($.sourcemaps.init())
     .pipe($.babel())
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/app/scripts'))
-    .pipe(reload({stream: true}));
+    .pipe(gulp.dest('.tmp/app/scripts'));
 });
-
-function lint(files, options) {
-  return () => {
-    return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
-      .pipe($.eslint(options))
-      .pipe($.eslint.format())
-      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-  };
-}
-
-const testLintOptions = {
-    env: {
-        mocha: true
-    }
-};
-
-const codeLintOptions = {
-    rules: {
-        "strict": "warning",
-        "quotes": "warning"
-    },
-    globals: {
-        'jQuery':false,
-        '$':true
-    }
-};
-
-gulp.task('lint', lint(config.jsFiles, codeLintOptions));
-gulp.task('lint:test', lint(config.jsTestFiles, testLintOptions));
 
 gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src(config.htmlFiles)
@@ -89,18 +54,6 @@ gulp.task('html', ['styles', 'scripts'], () => {
     .pipe(gulp.dest(config.dist))
     .pipe($.rev.manifest())
     .pipe(gulp.dest(config.dist));
-});
-
-gulp.task('images', () => {
-  return gulp.src(config.imageFiles)
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
-    .pipe(gulp.dest(config.dist + '/app/images'));
 });
 
 
@@ -120,59 +73,6 @@ gulp.task('extras', () => {
 });
 
 gulp.task('clean', del.bind(null, ['.tmp']));
-
-gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'src'],
-      routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
-  });
-
-  gulp.watch([
-    config.htmlFiles,
-    config.imageFiles,
-    '.tmp/app/fonts/**/*'
-  ]).on('change', reload);
-
-  gulp.watch(config.scssFiles, ['styles']);
-  gulp.watch(config.jsFiles, ['scripts']);
-  gulp.watch(config.fontFiles, ['fonts']);
-  gulp.watch('package.json', ['wiredep', 'fonts']);
-});
-
-gulp.task('serve:dist', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: [config.dist]
-    }
-  });
-});
-
-gulp.task('serve:test', ['scripts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/app/scripts',
-        '/node_modules': 'node_modules'
-      }
-    }
-  });
-
-  gulp.watch(config.jsFiles, ['scripts']);
-  gulp.watch(config.jsTestFiles).on('change', reload);
-  gulp.watch(config.jsTestFiles, ['lint:test']);
-});
 
 // inject NPM components
 gulp.task('wiredep', () => {
@@ -195,7 +95,7 @@ gulp.task('other', () => {
     .pipe(gulp.dest(config.otherDist));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['html', 'fonts', 'extras'], () => {
   return gulp.src(config.dist + '/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
